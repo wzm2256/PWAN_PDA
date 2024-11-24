5e43d8ebdb5052f3d6fd9ab5eeff9cc03b438159
lzm7788 <wzm2256@qq.com>
2024-11-24 10:15:11 +0100

    v0.12.1 1. add license 2. clean

--- models/D.py
+++ models/D.py
@@ -19,33 +19,3 @@ class D2(nn.Module):
 		out = -torch.abs(p_out)
 
 		return out
-
-
-
-# import torch.nn.parallel
-# import torch.utils.data
-# from torch.autograd import Variable
-# import numpy as np
-# import torch.nn.functional as F
-# from torch.nn.parameter import Parameter
-# import pdb
-
-# class Identity(nn.Module):
-# 	def __init__(self, dim):
-# 		super(Identity, self).__init__()
-#
-# 	def forward(self, x):
-# 		return x
-
-		# if norm_I == 0:
-		# 	my_norm = Identity
-		# elif norm_I == 1:
-		# 	my_norm = nn.LayerNorm
-
-# import torch
-# from torch.autograd import grad
-# import pdb
-
-# Pooling each PC.
-# standard network (standard pointnet)
-# not symmetric h
